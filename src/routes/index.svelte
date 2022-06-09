dba5365d7c9c3f2cc0d18c2b53bd6529a9670b7e
Juzer Zarif <juzerapj@gmail.com>
2022-06-09 12:09:59 -0500

    🔍️ (description): add meta description

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,4 +2,12 @@
 	import Home from './_home';
 </script>
 
+<svelte:head>
+	<title>Juzer Zarif</title>
+	<meta
+		name="description"
+		content="Juzer Zarif is a software engineer focused on web development. Contact him on his website."
+	/>
+</svelte:head>
+
 <Home />
